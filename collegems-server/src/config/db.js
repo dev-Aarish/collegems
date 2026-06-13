92de3876e882745c6aa3184b343ebb5b33fab933
Suhaskumard <suhaskumard4@gmail.com>
2026-06-13 12:33:30 +0530

    feat: implement Real-time Study Groups and collaborative features

--- collegems-server/src/config/db.js
+++ collegems-server/src/config/db.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const { MongoMemoryServer } = await import('mongodb-memory-server');
-    const mongoServer = await MongoMemoryServer.create();
-    await mongoose.connect(mongoServer.getUri());
-    console.log("MongoDB In-Memory Server Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (err) {
     console.error(err);
+    process.exit(1);
   }
 };
